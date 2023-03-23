3ce17ff64945990a128b946ad099b618a3a758ee
Replit user <>
2023-03-23 11:36:13 +0000

    String Manipulation

---
+++ day-1-1-exercise/Day-1/string_manipluation.py
@@ -0,0 +1,11 @@
+#Printing new Line
+print("Hello Python \n Hello Python \n Hello Python \n")
+
+#String Concatination option One
+print("Hello " + "Dagmawi")
+
+#String Concatination option Two
+print("Hello" + " Dagmawi")
+
+#String Concatination option Three
+print("Hello" + " " + "Dagmawi")
