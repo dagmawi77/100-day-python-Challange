806ea9fc985f60536ac02d056e7bf1971f28c41c
Replit user <>
2023-03-25 12:48:49 +0000

    primitive data type

---
+++ day-2/Primitive_data_type.py
@@ -0,0 +1,18 @@
+# String Data Type
+
+name = "Dagmawi"
+print(name[2])
+
+print(name)
+
+#Intiger Data Type
+
+print(300+600)
+
+#Float Data type
+
+print(355.45+200)
+
+#Boolen Data Type
+
+print(True)
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-#Fix the code below 👇
-print("Hello Python")
+# #Fix the code below 👇
+# print("Hello Python")
 
-name = input("What is Your Name? ")
+# name = input("What is Your Name? ")
 
-fClub = input("What is Your Fevorit FootBall Club ?")
+# fClub = input("What is Your Fevorit FootBall Club ?")
 
-print (name +" is " + fClub + " Club Fun")
+# print (name +" is " + fClub + " Club Fun")
 
-print(len(input("Type Your Name to Know the lenth of Your name ..")))
+# print(len(input("Type Your Name to Know the lenth of Your name ..")))
 
 
-last_name = input("What is your last name ?")
+# last_name = input("What is your last name ?")
 
-length = len(last_name)
+# length = len(last_name)
 
-print("Your Last name is .." + last_name + "Your name length is .." + str(length))
+# print("Your Last name is .." + last_name + "Your name length is .." + str(length))
+
+# String Data Type
+
+name = "Dagmawi"
+print(name[2])
+
+print(name)
+
+#Intiger Data Type
+
+print(300+600)
+
+#Float Data type
+
+print(355.45+200)
+
+#Boolen Data Type
+
+print(True)
 
