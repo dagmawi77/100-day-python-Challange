5bddfc0defe8d40e10d5e6a2d4b098488db740b0
Replit user <>
2023-03-25 07:51:36 +0000

    day-1-4-exercise

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ print (name +" is " + fClub + " Club Fun")
 print(len(input("Type Your Name to Know the lenth of Your name ..")))
 
 
+last_name = input("What is your last name ?")
+
+length = len(last_name)
+
+print("Your Last name is .." + last_name + "Your name length is .." + str(length))
+
