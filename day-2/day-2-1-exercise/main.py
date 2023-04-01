abe9f0ff237fa41224665952c6498580d52270d3
Replit user <>
2023-04-01 10:22:27 +0000

    Second Solution

--- day-2/day-2-1-exercise/main.py
+++ day-2/day-2-1-exercise/main.py
@@ -7,3 +7,18 @@ num_one = int(two_digit_number[0])
 num_two = int(two_digit_number[1])
 print(num_one + num_two)
 
+
+
+#second Solution
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+
+result = first_num + second_num
+print(result)
+####################################
+#Write your code below this line 👇
