e9770e68e5069cac900f05dce42872fad410b912
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-06 06:49:32 +0000

    Recap Yesterday Concept

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,1 +1,13 @@
-print("Hello Dagi")
+#the Concep Printing the String
+print("Hello Programer")
+#String Manipulation Example
+print(
+  "Hello Programer Dagmawi \nHello Programer Dagmawi \nHello Programer Dagmawi \n"
+)
+#String Conactination
+print("Hello" + " " + "Programer" + " " + "Dagmawi")
+#How To print other symobl
+print("'The Best is come to yet' - By Dagmawi")
+# other example
+
+print('"never giveup"- by Dagmawi')
