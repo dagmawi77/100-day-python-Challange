6ff1e12e906cd2d29b51c940baedffeacd342781
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-07 19:03:53 +0000

    Day One Challenge

--- main.py
+++ main.py
@@ -1,13 +1,68 @@
-#Fix the code below 👇
+# # #Fix the code below 👇
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
-# Input From User
+# # print("Day 1 - String Manipulation")
+# # print('String Concatenation is done with the "+" sign.')
+# # print('e.g. print("Hello " + "world")')
+# # print("New lines can be created with a backslash and n.")
+# # # Input From User
 
-name = input("What is You Name?  ")
-print(len(name))
-print("Your name is .." + name + " have " + " Character")
+# # name = input("What is You Name?  ")
+# # print(len(name))
+# # print("Your name is .." + name + " have " + " Character")
 
-print(len(input("Second Name? ")))
+# # print(len(input("Second Name? ")))
+
+# # # variale dicalartion
+
+# # fname = input("What is You Father name? ..")
+# # fnamenum = len(fname)
+
+# # print(fnamenum)
+
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# # ####################################
+# # #Write your code below this line 👇
+# # c = a
+# # a = b
+
+# # b = c
+
+# # c = a
+
+# # a = b
+
+# # b = c
+
+# c = a
+# d = b
+
+# a = d
+# b = c
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+# Project For Band Name Generator
+
+# Welcoming Your User By promting how the App Works
+print("Welcome Band Name Generator Apps")
+
+#input Your Born Address
+
+born = input("Where Do You Born please enter the City ? \n")
+
+#second Input the User Dog Name
+
+dog_name = input("Please Enter Your Dog name ? \n")
+
+#Concatinate The input with Additional Message
+
+print("Your Band Name is ......." + born + " " + dog_name)
