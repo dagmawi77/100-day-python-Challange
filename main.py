7b25e159ea66413b72451e10fe9a1c2b79f8882d
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-07 13:55:00 +0000

    Variable declaration

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+# Input From User
+
+name = input("What is You Name?  ")
+print(len(name))
+print("Your name is .." + name + " have " + " Character")
+
+print(len(input("Second Name? ")))
