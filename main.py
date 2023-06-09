3f726c19a1b9f75c0393070876a9ceb441907e53
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-09 19:25:03 +0000

    Day 2 Exercise

--- main.py
+++ main.py
@@ -1,103 +1,147 @@
-# # #Fix the code below 👇
+# recup all last day Concepts
+# first about how can we print any statment
 
-# # print("Day 1 - String Manipulation")
-# # print('String Concatenation is done with the "+" sign.')
-# # print('e.g. print("Hello " + "world")')
-# # print("New lines can be created with a backslash and n.")
-# # # Input From User
+print("Print Any Statment")
 
-# # name = input("What is You Name?  ")
-# # print(len(name))
-# # print("Your name is .." + name + " have " + " Character")
+# how can we Concatinate the Strings in print
 
-# # print(len(input("Second Name? ")))
+print("Adding Two Statments like" + "......" + "This")
 
-# # # variale dicalartion
+#String manipulation
 
-# # fname = input("What is You Father name? ..")
-# # fnamenum = len(fname)
+# it all About String manipulation On python
+# the first  Step is String Conactination It all about Combine Two Or more String on one here is the exampel
 
-# # print(fnamenum)
+str1 = "Hello"
+str2 = "Python"
+# the Plus Sign is help To Concatinate the string
+result = str1 + " " + str2
 
-# # 🚨 Don't change the code below 👇
-# a = input("a: ")
-# b = input("b: ")
-# # 🚨 Don't change the code above 👆
+print(result)
 
-# # ####################################
-# # #Write your code below this line 👇
-# # c = a
-# # a = b
+# the Second mainpulation Methood is slicing it about get some of the portion from the declared String
 
-# # b = c
+myString = "Hello World"
+# any programing is Counting From 0 so you get Hello Wold only by below command
+print(myString[0:5])
 
-# # c = a
+print(myString[6:])
+# It print the reverse String based on Sliced postion
+print(myString[:5:-1])
 
-# # a = b
+# the Thired String Manipulation is replacing it help to Modifed the String by Replacing Spacific Word
 
-# # b = c
+myStringOne = "Hello World"
+newString = myStringOne.replace("World", "Python3")
+print(newString)
 
-# c = a
-# d = b
+# The Forth One is String Formating this is Hlep to Get Daynamic String
 
-# a = d
-# b = c
+fname = input("Can You Enter Your First Name ? \n")
+age = input("Can You Enter You Age \n")
 
-# #Write your code above this line 👆
-# ####################################
+ageConverter = str(age)
 
-# # 🚨 Don't change the code below 👇
-# print("a: " + a)
-# print("b: " + b)
+# There are 3 Type of String Formating % mode str.format and f-string Mode
 
-# Project For Band Name Generator
+#The % mode Example
+message = "The User name is %s and the Age is %s" % (fname, ageConverter)
+print(message)
 
-# Welcoming Your User By promting how the App Works
-# print("Welcome Band Name Generator Apps")
+name = "Alice"
+age = 25
+message = "My name is %s and I am %d years old." % (name, age)
+print(message)  # Output: My name is Alice and I am 25 years old.
 
-# #input Your Born Address
+#The str.format method
 
-# born = input("Where Do You Born please enter the City ? \n")
+messageTwo = "The User Name is {} and his Age is {}".format(name, age)
+print(messageTwo)
 
-# #second Input the User Dog Name
+#The f-string Method
 
-# dog_name = input("Please Enter Your Dog name ? \n")
+messageThree = f"The User Name is {name} and his Age is also {age}"
 
-# #Concatinate The input with Additional Message
+print(messageThree)
 
-# print("Your Band Name is ......." + born + " " + dog_name)
+# About Variable  is like phonebook that is about reference or Additional name of the Existing
 
-# it all about data type
+variableName = "Holding This Message"
 
-#print the index of Charator W
+print(variableName)
 
-# print("Hello World"[6])
+# variable Naming we can give the name of Variable Strting with letter or _
+variable_Name = "This Is Valid Variable"
+_Var = "This Is also Valid Valiable"
+var_1 = "This Alos other Valid Varibale"
+# 5642FGSFGS="gsdgdsja" this Kind of Variable
+# %$62="This is also Not valid Variable"
 
-# #String Data type
+# About Data Type is means The the Variable that hold what kind of Data type like integer,String and Boool and Float
 
-# print ("Hello " + "Python")
+# Python is Dynamicly typed languge It means You can't Defined the data type before variable name it automaicaly know the type
 
-# #intiger Data Type
+# String Data Type
 
-# print(444+555)
+strType = "This Is String Data Type"
 
-# #Float Data Type
+print(type(strType))
 
-# print(34356.22 +10.44)
+#integer Data Type
 
-# #Boolean Data Type
+intType = 1001
 
-# print(True)
+print(type(intType))
 
-# Data Type Finder and Data type Convertor
+# float Data Type
 
-name = input("What is Your Name? ..")
+floatType = 345.22
 
-name_len = str(len(name))
+print(type(floatType))
 
-print("the Input Name " + name + "have " + name_len + " Charactor")
+# Bool data Type
 
-print(type("Something"))
-print(type(3252))
-print(type(True))
-print(type(42534.253462))
+boolType = True
+
+print(type(boolType))
+
+# Type Convertion converting int to String
+
+intType1 = 355
+strConv = str(intType1)
+print(type(strConv))
+
+# Converting String To int
+
+strType1 = "300"
+intConv = int(strType1)
+
+print(type(intConv))
+print(intConv)
+
+# You can Convert Any Type Of data if it is possible
+
+#The Variable Callenge It take 2 number and swap the value
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+print(type(two_digit_number))
+
+numOne = int(two_digit_number[0])
+numTwo = int(two_digit_number[1])
+
+print(numOne + numTwo)
