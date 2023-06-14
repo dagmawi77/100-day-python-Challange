b1d7d11bc9d863ce88174da6a020e8a476e9e710
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-14 18:50:23 +0000

    nested if else

--- day-3/if_else.py
+++ day-3/if_else.py
@@ -3,6 +3,13 @@ print("The Rule For Air port Employeement")
 hight = float(input("Input Your Hight with cm  "))
 
 if hight >= 1.70:
-    print("You can Work with Us")
+  print("You can Work with Us")
+  age = int(input("How Old Are You ? "))
+  if age >= 30:
+    print("You pass your hight but you so old")
+  elif age >= 50:
+    print("I think you need pention")
+  else:
+    print("Sucussfuly Hired Welcome to Airlines")
 else:
-    print("we don't place for you ")
+  print("we don't place for you ")
