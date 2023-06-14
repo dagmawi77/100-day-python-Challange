1269b8623d19e51184591733283fab858bddd7f0
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-14 18:21:20 +0000

    if_else

---
+++ day-3/if_else.py
@@ -0,0 +1,8 @@
+print("The Rule For Air port Employeement")
+
+hight = float(input("Input Your Hight with cm  "))
+
+if hight >= 1.70:
+    print("You can Work with Us")
+else:
+    print("we don't place for you ")
