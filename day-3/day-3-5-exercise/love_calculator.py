93d2bdb81ed0b9f67595c8b85e03c4faaad6be52
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-16 17:14:25 +0000

    love calculator

--- day-3/day-3-5-exercise/love_calculator.py
+++ day-3/day-3-5-exercise/love_calculator.py
@@ -29,3 +29,36 @@ elif true_love_int >= 40 and true_love_int <= 50:
   print(f"Your score is {true_love_int}, you are alright together.")
 else:
   print(f"Your score is {true_love_int}.")
+
+# option Two
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+full_name = name1.lower() + name2.lower()
+t = full_name.count("t")
+r = full_name.count("r")
+u = full_name.count("u")
+e = full_name.count("e")
+true = t + r + u + e
+l = full_name.count("l")
+o = full_name.count("o")
+v = full_name.count("v")
+e = full_name.count("e")
+love = l + o + v + e
+
+true_love_str = str(true) + str(love)
+
+true_love_int = int(true_love_str)
+
+if true_love_int < 10 or true_love_int > 90:
+  print(
+    f"Your score is {true_love_int}, you go together like coke and mentos.")
+elif true_love_int >= 40 and true_love_int <= 50:
+  print(f"Your score is {true_love_int}, you are alright together.")
+else:
+  print(f"Your score is {true_love_int}.")
