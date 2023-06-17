00f8b6fa0fc147c8c76bf220e8cc9be25de7eb24
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-17 14:35:58 +0000

    random

---
+++ day-4/main.py
@@ -0,0 +1,11 @@
+import random
+import my_moudule
+
+random_int = random.randint(0, 10)
+random_string =random_int
+print(f"The Rocket Lunch in {random_string}")
+
+random_float = random.random() * 500
+print(random_float)
+
+print(my_moudule.pi)
