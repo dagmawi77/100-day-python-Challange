f9af80c1e51221c5dc24e94fe8c1fee0bdef955e
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-17 14:39:15 +0000

    love Calcuator

--- day-4/main.py
+++ day-4/main.py
@@ -2,10 +2,14 @@ import random
 import my_moudule
 
 random_int = random.randint(0, 10)
-random_string =random_int
+random_string = random_int
 print(f"The Rocket Lunch in {random_string}")
 
 random_float = random.random() * 500
 print(random_float)
 
 print(my_moudule.pi)
+
+love_calculater = str(random.randint(50, 100))
+
+print(f"You Lover Her {love_calculater}")
