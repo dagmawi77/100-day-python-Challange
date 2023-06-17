849257bba3be1c9bbdbe3a8131f52db5f76ae3b9
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-17 14:57:38 +0000

    head tail Exercise

---
+++ day-4/day-4-1-exerice/day-4-1exerices.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_num = random.randint(0, 2)
+
+if random_num == 1:
+  print("ሰው")
+elif random_num == 0:
+  print("ጎፈር")
+else:
+  print("መሃል")
