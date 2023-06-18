85ceaf0c51c9b0098e8ad63afe543e056ce76538
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-18 05:34:32 +0000

    List Example

---
+++ day-4/list.py
@@ -0,0 +1,9 @@
+ethio_city = ["addis ababa", "Adama", "Baher Dar", "Dire Dawa"]
+
+print(ethio_city[0])
+
+user_city_name = input("Can You list All city Name in Ethiopia ?")
+
+ethio_city.append(user_city_name)
+
+print(ethio_city)
