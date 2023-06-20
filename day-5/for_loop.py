e77575e6fcde47a4b9971cae58c2f4008e32a429
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-20 18:33:55 +0000

    for loop

---
+++ day-5/for_loop.py
@@ -0,0 +1,9 @@
+# This Is example of For loop
+firutes = ["Banana", "Appel", "Mango", "Pear"]
+
+for firute in firutes:
+  print(firute)
+  print(f"The firute list..{firute}")
+  print(firutes)
+# The Indentation is matters for loop
+print(firutes)
