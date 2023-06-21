86e7d635346ba81d4b48c734972d12f68cb838ec
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-21 18:22:01 +0000

    password-generator

---
+++ day-5/password-generator/password-generator.py
@@ -0,0 +1,51 @@
+#Password Generator Project
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+get_letters = []
+
+for _ in range(0, nr_letters):
+    random_letter = random.randint(0, len(letters) - 1)
+    get_letters.append(letters[random_letter])
+get_nummbers = []
+for _ in range(0,nr_numbers):
+  random_number = random.randint(0,len(numbers) -1)
+  get_nummbers.append(numbers[random_number])
+get_symbols =[]
+for _ in range(0,nr_symbols):
+  random_symbol = random.randint(0,len(symbols)-1)
+  get_symbols.append(symbols[random_symbol])
+join_pass = get_letters + get_nummbers + get_symbols
+
+join_password = "".join(get_letters) + "".join(get_nummbers) + "".join(get_symbols)
+
+print(join_pass)
+random.shuffle(join_pass)
+
+print(join_pass)
+
+join_pass_final = "".join(join_pass)
+
+print(f"Your Password is ... {join_pass_final}")
+# shiffle_pass = random.shuffle(join_pass,len(join_pass))
+# print(shiffle_pass)
+
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
