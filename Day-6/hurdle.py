ad866373e4a915444331856a409b2c839ac8c90a
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-22 08:27:57 +0000

    v2

--- Day-6/hurdle.py
+++ Day-6/hurdle.py
@@ -35,3 +35,24 @@ four_step()
 # WARNING: Do not change this comment.
 # Library Code is below.
 ################################################################
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def four_step():
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+def three_step():
+    turn_left()
+    move()
+    turn_left()
+move()
+turn_left()
+for step in range(0,6):
+    four_step()
+    three_step()
