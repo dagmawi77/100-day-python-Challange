6c51873255dd0b6bdc3934344dc9ccd7b9d17737
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-22 08:12:56 +0000

    Hurdle Game Solution

---
+++ Day-6/hurdle.py
@@ -0,0 +1,37 @@
+def turn_right():
+  turn_left()
+  turn_left()
+  turn_left()
+
+
+def four_step():
+  move()
+  turn_right()
+  move()
+  turn_right()
+  move()
+
+
+def three_step():
+  turn_left()
+  move()
+  turn_left()
+
+
+move()
+turn_left()
+four_step()
+three_step()
+four_step()
+three_step()
+four_step()
+three_step()
+four_step()
+three_step()
+four_step()
+three_step()
+four_step()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
