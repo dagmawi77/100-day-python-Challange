9e46d2447fed3c4f318370346bbd2bea9854ec16
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-23 07:55:23 +0000

    version 2 final

--- Day-7/hangman_game_start v2 .py
+++ Day-7/hangman_game_start v2 .py
@@ -12,18 +12,22 @@ print(f'Pssst, the solution is {chosen_word}.')
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
+display = []
+word_range = len(chosen_word)
+for _ in chosen_word:
+  display.append("_")
+print(display)
+
 guess = input("Guess a letter: ").lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-get_letter = []
-for letter in chosen_word:
+for position in range(word_range):
+  letter = chosen_word[position]
   if letter == guess:
-    get_letter.append(letter)
-  else:
-    get_letter.append("_")
-print(get_letter)
+    display[position] = letter
 
+print(display)
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
