400e4f86969f23652433ac7f6eafce3c5f7f97ac
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-23 07:34:59 +0000

    Version Two of The Game

--- Day-7/hangman_game_start.py
+++ Day-7/hangman_game_start.py
@@ -4,7 +4,7 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+chosen_word = random.choice(word_list)
 print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 user_letter = input("Guess a letter :  ").lower()
