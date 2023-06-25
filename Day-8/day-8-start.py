a33fa30069e782d2dd7e8c337d0d590342e62013
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-25 10:19:17 +0000

    Keyword Argument

--- Day-8/day-8-start.py
+++ Day-8/day-8-start.py
@@ -7,16 +7,26 @@
 #   print("Python")
 #   print("Function")
 
-
 # # Call the greet() function and run your code.
 
 # greet()
 
+
 def greeting_with_parameter(name):
   print(f"Hello {name}")
-  print(f"How are You today {name}")
+  print(f"How are You today {name}?")
+
 
 fname = input("Please Enter Your name?  ")
 
 greeting_with_parameter(fname)
-  
+
+
+def greeting_with(name, lname, age):
+  print(f"Hello {name} {lname} You are {age} years old")
+
+
+# This is possitional Argument
+greeting_with("Dagmawi", "Lettarik", 25)
+# This is keyword argument
+greeting_with(lname="Leta", age=26, name="Dagi")
