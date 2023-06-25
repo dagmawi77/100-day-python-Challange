8dfc16d6839564ea02a3627cca80d3476386b585
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-25 09:43:34 +0000

    Starting Day 8 function

---
+++ Day-8/day-8-start.py
@@ -0,0 +1,13 @@
+# Review:
+# Create a function called greet().
+def greet():
+
+  # Write 3 print statements inside the function.
+  print("Hello")
+  print("Python")
+  print("Function")
+
+
+# Call the greet() function and run your code.
+
+greet()
