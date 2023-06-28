fb27e55c057bfcc17e4e36d0ca4a0ab659774000
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-28 08:59:41 +0000

    caesar-cipher final

--- Day-8/caesar-cipher-1-start.py
+++ Day-8/caesar-cipher-1-start.py
@@ -7,28 +7,35 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# Adding the Algorithm first
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 
 def encrypt(text, shift):
-  split_text = []
-  for index, items in enumerate(alphabet):
-    for letter in text:
-      split_text.append(letter)
-      if letter == items:
-        print(items, index)
-        print(items[index + shift])
 
+  #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+  #e.g.
+  #plain_text = "hello"
+  #shift = 5
+  #cipher_text = "mjqqt"
 
-encrypt(text, shift)
+  #print output: "The encoded text is mjqqt"
+  plain_text = []
+  cipher_text = []
+  cipher_text_final = []
+  for letter in text:
+    for index, iteam in enumerate(alphabet):
+      # print(index,iteam)
+      if iteam == letter:
+        plain_text.append(iteam)
+        cipher_text.append(index + shift)
+  for cipher_list in cipher_text:
+    list_cip = int(cipher_list)
+    # print(list_cip)
+    cipher_text_final.append(alphabet[list_cip])
 
-#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-#e.g.
-#plain_text = "hello"
-#shift = 5
-#cipher_text = "mjqqt"
-#print output: "The encoded text is mjqqt"
+  print("".join(plain_text))
+  print("".join(cipher_text_final))
+
 
 ##HINT: How do you get the index of an item in a list:
 #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
@@ -36,3 +43,5 @@ encrypt(text, shift)
 ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+encrypt(text, shift)
