8f21f6c2e8364dafa602cecbeaedf42e144707b6
dagmawi77 <dagmawiletarik@gmail.com>
2023-06-30 12:53:29 +0000

    nested_dictionary.py

---
+++ Day-9/nested_dictionary.py
@@ -0,0 +1,28 @@
+
+city_visited = {
+  "France": {
+    5000: "paris",
+    64747: "Lile",
+    6546: "Djong"
+  },
+  "Ethiopia": {
+    "visted_place": ["Addis Ababa", "Gonder", "Baher Dar", "jema", ""],
+    "total visitor": 5243646
+  }
+}
+
+print(city_visited["Ethiopia"])
+
+# Nest the Dictonary into list
+
+info = [{
+  "Contry": "Ethiopia",
+  "city": ["addis abab", "baher dar", "jema", "Adama"],
+  "number of vistor": 1000
+}, {
+  "Contry": "Egypt",
+  "city": ["Kayro", "Aswan", "Giza"],
+  "number of vistor": 1000
+}]
+
+print(info)
