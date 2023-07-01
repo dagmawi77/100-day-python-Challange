cec0115e8e7e259b038008f63392597a17d075d5
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-01 09:07:04 +0000

    Multiple output

--- Day-10/day-10-start.py
+++ Day-10/day-10-start.py
@@ -1,9 +1,13 @@
 def format_name(f_name, l_name):
+  # multiple return statment
+  if f_name == "" and l_name == "":
+    return "You Insert Nothing"
   f_name = f_name.title()
   l_name = l_name.title()
   full_name = f_name + " " + l_name
   return full_name
 
 
-formated_string = format_name("DAGMAWI", "LETARIK")
+formated_string = format_name(input("What is Your first name? "),
+                              input("What is Your Last Name ? "))
 print(formated_string)
