252e472ed42f96ecd3ee281102a96f2b1bab85ec
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-01 11:56:17 +0000

    day-10-1-exercise solution 2

--- Day-10/day-10-1-exercise/main.py
+++ Day-10/day-10-1-exercise/main.py
@@ -10,15 +10,19 @@ def is_leap(year):
   else:
     return False
 
+
 def days_in_month(year, month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  for index,new_month in enumerate(month_days):
-    final_new_month = int(month - 1)
-    if index == final_new_month:
-      return month_days[index]
-      
-  
-#🚨 Do NOT change any of the code below 
+  # for index,new_month in enumerate(month_days):
+  #   final_new_month = int(month - 1)
+  #   if index == final_new_month:
+  #     return month_days[index]
+  if is_leap(year) and month == 2:
+    return 29
+  print(month_days[month - 1])
+
+
+#🚨 Do NOT change any of the code below
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
