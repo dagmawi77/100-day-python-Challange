62b99bfa4b239050242b8cce9a9f9048124aff5e
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-02 06:49:01 +0000

    Adding Doc String in my Code

--- Day-10/day-10-start.py
+++ Day-10/day-10-start.py
@@ -1,6 +1,13 @@
 def format_name(f_name, l_name):
+  """ Adding Doc String For Function the above faction is used for
+  formaing the name like this => Dagmawi Letarik
+  """
   # multiple return statment
   if f_name == "" and l_name == "":
+    """
+    Or it Used For multi line commenting 
+    also
+    """
     return "You Insert Nothing"
   f_name = f_name.title()
   l_name = l_name.title()
@@ -11,3 +18,4 @@ def format_name(f_name, l_name):
 formated_string = format_name(input("What is Your first name? "),
                               input("What is Your Last Name ? "))
 print(formated_string)
+format_name()
