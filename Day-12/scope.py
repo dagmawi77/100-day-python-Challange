729e40aa425bcc5e39b8d00e3a2a165eb9eae69c
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-04 13:31:15 +0000

    Adding Block Scope Concept

--- Day-12/scope.py
+++ Day-12/scope.py
@@ -10,3 +10,25 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local Scope
+
+
+def local_scope():
+  local_scope_variable = "I am local"
+  print(local_scope_variable)
+
+
+local_scope()
+
+# if you call variable out side of scop like this print(local_scope_variable)
+# print(local_scope_variable)
+
+# There is No Block scope
+level = 3
+enemies = ["Skelton", "Zumba", "Alian"]
+
+if level < 5:
+  new_enemie = enemies[0]
+
+print(new_enemie)
