370e778fe516fc4f03412a3beb7507eac8df3b9b
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-05 07:32:11 +0000

    Additional way for modified global scope

--- Day-12/modified_global_scope.py
+++ Day-12/modified_global_scope.py
@@ -1,3 +1,28 @@
+################### Scope ####################
+
+# enemies = 1
+
+# def increase_enemies():
+#   enemies = 2
+
+  
+#   print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+
+# There is No Block scope
+# level =3
+# enemies = ["Skelton","Zumba","Alian"]
+
+# if level < 5:
+#   new_enemie = enemies[0]
+
+# print(new_enemie)
+
+# Modifing Global scope
+
 modified = 1
 
 def modfied_function():
@@ -9,11 +34,15 @@ def modfied_function():
 modfied_function()
 print(f"Modifed Number is {modified}")
 
-
-
 # other way of modifing global scope
 
 md = 10
 
 def md_function():
-  print(f"Modifed Number is {modified}")
+  print(f"Modifed Number is {md}")
+  return md+10
+
+md_function()
+print(f"Modifed Number is {md}")
+md_final = md_function();
+print(md_final)
