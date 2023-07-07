620e54d0fa52d3a95eadcbde390ead02418b4b40
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-07 10:48:59 +0000

    Band Name generator App

---
+++ recup/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome To band Name Generator App")
+pitName = input("What is You'r Pit name? ").title()
+name = input("What is You Name? ").title()
+
+print(f"Your Band Name is {pitName}{name}")
