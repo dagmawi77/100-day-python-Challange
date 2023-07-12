27922d3169e37b29948a3a3c0adc73b21582fb07
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-12 12:12:05 +0000

    Update the Game Functionality

--- Day-14/main.py
+++ Day-14/main.py
@@ -3,35 +3,41 @@ import random
 from art import logo, vs
 from game_data import data
 
+from replit import clear
+
 # printing basic interface
 print(logo)
-choise_a = random.choice(data)
 
-print(
-  f"Compare A: {choise_a['name']} a {choise_a['description']} , from {choise_a['country']}"
-)
-
-print({choise_a['follower_count']})
-
-print(vs)
-
-choise_b = random.choice(data)
-print(
-  f"Compare B: {choise_b['name']} a {choise_b['description']} , from {choise_b['country']}"
-)
-print({choise_b['follower_count']})
-
+life = 0
 continue_game = True
 while continue_game:
+  choise_a = random.choice(data)
+
+  print(
+    f"Compare A: {choise_a['name']} a {choise_a['description']} , from {choise_a['country']}"
+  )
+
+  print({choise_a['follower_count']})
+
+  print(vs)
+
+  choise_b = random.choice(data)
+  print(
+    f"Compare B: {choise_b['name']} a {choise_b['description']} , from {choise_b['country']}"
+  )
+  print({choise_b['follower_count']})
 
   user_guess = input("Who has More Follower? Type 'A' or B ").capitalize()
 
   if choise_a['follower_count'] > choise_b[
       'follower_count'] and user_guess == "A":
-    print("User win")
+    life += 1
+    print(f"your right current score {life}")
   elif choise_b['follower_count'] > choise_a[
       'follower_count'] and user_guess == "B":
-    print("User win")
+    life += 1
+    print(f"your right current score {life}")
   else:
-    print("user lost")
     continue_game = False
+    clear()
+    print(f"sorry , that is wrong.Final Score: {life}")
