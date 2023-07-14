62c7e1078e20049d7e983fc95576361ff0d5e757
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-14 22:06:39 +0300

    adding Menu

--- Day-15/main.py
+++ Day-15/main.py
@@ -1,3 +1,32 @@
-# adding 3 favourite of Drink
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-chose_drink = input("What would you like? (espresso/latte/cappuccino):")
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
