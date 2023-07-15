7e3ca0e8c4ede9b13cebd9ed5e062439083fb766
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-07-15 22:24:35 +0300

    Update main.py

--- Day-15/main.py
+++ Day-15/main.py
@@ -53,4 +53,4 @@ elif user_choies == "espresso" or user_choies == "latte" or user_choies == "capp
 
     change = (quarters * quarters_value) + (dimes * dimes_value) + (nickles * nickles_value) + (pennies * pennies_value)
     print(f"here is change ${(change)}")
-
+    print("new thing")
