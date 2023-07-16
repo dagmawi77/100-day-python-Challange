8cd6f343c4d85318a4f5ca81b750f4bcb6f6188e
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-16 22:31:57 +0300

    Adding off functionality

--- Day-15/main.py
+++ Day-15/main.py
@@ -24,7 +24,7 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+profite = 0
 resources = {
     "water": 300,
     "milk": 200,
@@ -32,25 +32,34 @@ resources = {
 }
 
 
-user_choies = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
 
 quarters = 0.25
 dimes = 0.10
 nickles = 0.05
 pennies = 0.01
 
+game_countinue = True
+while game_countinue:
+    user_choies = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_choies == "off":
+        print("Game Over")
+        game_countinue = False
+    elif user_choies == "report":
+        for index in resources:
+            print(f"{index} : {resources[index]}")
+        print(f"Money : {profite} ")
+#         print(f"Water : {resources['water']}")
+#         print(f"Milk : {resources['milk']} " )
+#         print(f"Coffee : {resources['coffee']} ")
+#         print(f"Money : {profite} ")
+    elif user_choies == "espresso" or user_choies == "latte" or user_choies == "cappuccino":
+        print("please Enter Coin.")
+        quarters_value = int(input("How many quarters? : "))
+        dimes_value = int(input("How many dimes? : "))
+        nickles_value = int(input("How many nickles? : "))
+        pennies_value = int(input("How many quarters? : "))
 
-if user_choies == "report":
-    for index in resources:
-        print(f"{index} : {resources[index]}")
-elif user_choies == "espresso" or user_choies == "latte" or user_choies == "cappuccino":
-    print("please Enter Coin.")
-    quarters_value = int(input("How many quarters? : "))
-    dimes_value = int(input("How many dimes? : "))
-    nickles_value = int(input("How many nickles? : "))
-    pennies_value = int(input("How many quarters? : "))
 
-
-    change = (quarters * quarters_value) + (dimes * dimes_value) + (nickles * nickles_value) + (pennies * pennies_value)
-    print(f"here is change ${(change)}")
-    print("new thing")
+        change = (quarters * quarters_value) + (dimes * dimes_value) + (nickles * nickles_value) + (pennies * pennies_value)
+        print(f"here is change ${(change)}")
