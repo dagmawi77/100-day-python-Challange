246231760043dd98819f3ba28bebd41138a417ab
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-17 21:55:52 +0300

    turtle method

--- Day-16/main.py
+++ Day-16/main.py
@@ -7,8 +7,12 @@ dagi = Turtle()
 print(dagi)
 dagi.shape("turtle")
 dagi.color("red")
+dagi.position()
+dagi.forward(300)
+dagi.speed(10)
 
 my_screen = Screen()
 
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
