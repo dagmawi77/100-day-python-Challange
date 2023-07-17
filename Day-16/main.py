85b2963ead6cdfbfa836d8660d9f5fd45824513d
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-17 21:48:54 +0300

    day 16 exercise

---
+++ Day-16/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle,Screen
+# import another_module
+#
+# print(another_module.another_variable)
+
+dagi = Turtle()
+print(dagi)
+dagi.shape("turtle")
+dagi.color("red")
+
+my_screen = Screen()
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
