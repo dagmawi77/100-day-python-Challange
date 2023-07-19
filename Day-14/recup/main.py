f994d928dfca64a4fb58acc1f4ec892dea89dcd1
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-19 22:29:49 +0300

    adding functionality

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -4,11 +4,29 @@
 from art import logo, vs
 from game_data import data
 import random
+import replit
 #first print all game material
 print(logo)
 
-choice_one = random.choice(data)
 
-print(choice_one)
+game_continue = True
+score = 0
+while game_continue:
 
-print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
+    choice_one = random.choice(data)
+    choice_two = random.choice(data)
+    if choice_one == choice_two:
+        choice_two = random.choice(data)
+    print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
+    print(vs)
+    print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
+    user_choice = input("Who has more follower 'A' or 'B' ").capitalize()
+    if choice_one['follower_count'] > choice_two['follower_count'] and user_choice == "A":
+        score += 1
+        print(f"Your right current score {score}")
+    elif choice_one['follower_count'] < choice_two['follower_count'] and user_choice == "B":
+        score += 1
+        print(f"Your right current score {score}")
+    else:
+        print(f"Sorry, that is wrong. Final Score {score} ")
+        game_continue = False
