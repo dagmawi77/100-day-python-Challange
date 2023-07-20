b050f22cd526fe96e2abc76bf7a3489ee50b29ad
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-20 12:13:47 +0300

    user choies Comp

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -10,13 +10,21 @@ print(logo)
 choice_one = random.choice(data)
 print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
 
+print(vs)
 choice_two = random.choice(data)
 
 if choice_one == choice_two:
     choice_two = random.choice(data)
 print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
 
+user_choice = input("Who has more follower Type : 'A' or 'B' ").capitalize()
 
+if choice_one['follower_count']>choice_two['follower_count'] and user_choice=="A":
+    print("You Win")
+elif choice_one['follower_count']<choice_two['follower_count'] and user_choice=="B":
+    print("You Win also")
+else:
+    print("You lost")
 
 
 
