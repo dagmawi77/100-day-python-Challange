2c6f3420b8238ba15f5991f79d4e9a29b6d4c1b9
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-20 12:25:35 +0300

    game repitatve function

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -6,25 +6,29 @@ from game_data import data
 import random
 #first print all game material
 print(logo)
+game_continue = True
+score = 0
+while game_continue:
+    print(f"You are right! Current Score {score}")
+    choice_one = random.choice(data)
+    print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
 
-choice_one = random.choice(data)
-print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
-
-print(vs)
-choice_two = random.choice(data)
-
-if choice_one == choice_two:
+    print(vs)
     choice_two = random.choice(data)
-print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
 
-user_choice = input("Who has more follower Type : 'A' or 'B' ").capitalize()
+    if choice_one == choice_two:
+        choice_two = random.choice(data)
+    print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
 
-if choice_one['follower_count']>choice_two['follower_count'] and user_choice=="A":
-    print("You Win")
-elif choice_one['follower_count']<choice_two['follower_count'] and user_choice=="B":
-    print("You Win also")
-else:
-    print("You lost")
+    user_choice = input("Who has more follower Type : 'A' or 'B' ").capitalize()
+
+    if choice_one['follower_count']>choice_two['follower_count'] and user_choice=="A":
+        score += 1
+    elif choice_one['follower_count']<choice_two['follower_count'] and user_choice=="B":
+        score += 1
+    else:
+        game_continue = False
+        print(f"Sorry, that is wrong. final score {score}")
 
 
 
