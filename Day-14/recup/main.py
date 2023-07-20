61d5545eea72bdc6c398778f813d43cf6f0cc186
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-20 12:03:30 +0300

    Adding User choies

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -8,7 +8,15 @@ import random
 print(logo)
 
 choice_one = random.choice(data)
-
-print(choice_one)
-
 print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
+
+choice_two = random.choice(data)
+
+if choice_one == choice_two:
+    choice_two = random.choice(data)
+print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
+
+
+
+
+
