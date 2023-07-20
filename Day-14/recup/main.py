6f870aa57bd60f5dc0f816f85844f41981650705
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-20 14:33:20 +0300

    fix clear issue

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -10,24 +10,29 @@ print(logo)
 game_continue = True
 score = 0
 while game_continue:
-    print(f"You are right! Current Score {score}")
     choice_one = random.choice(data)
     choice_two = random.choice(data)
     if choice_one == choice_two:
         choice_two = random.choice(data)
-    print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
+    print(
+        f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} "
+    )
     print(vs)
-    print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
-    user_choice = input("Who has more follower Type : 'A' or 'B' ").capitalize()
+    print(
+        f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} "
+    )
+    user_choice = input(
+        "Who has more follower Type : 'A' or 'B' ").capitalize()
     clear()
     print(logo)
-    if choice_one['follower_count']>choice_two['follower_count'] and user_choice=="A":
+    if choice_one['follower_count'] > choice_two[
+            'follower_count'] and user_choice == "A":
         score += 1
-    elif choice_one['follower_count']<choice_two['follower_count'] and user_choice=="B":
+        print(f"You're right! Current score: {score}.")
+    elif choice_one['follower_count'] < choice_two[
+            'follower_count'] and user_choice == "B":
         score += 1
+        print(f"You're right! Current score: {score}.")
     else:
         game_continue = False
         print(f"Sorry, that is wrong. final score {score}")
-
-
-
