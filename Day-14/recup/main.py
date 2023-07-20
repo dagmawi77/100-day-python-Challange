586353f8d9dc3f4458071b8128e2840f772ab958
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-20 14:10:52 +0300

    add clear functinality

--- Day-14/recup/main.py
+++ Day-14/recup/main.py
@@ -4,6 +4,7 @@
 from art import logo, vs
 from game_data import data
 import random
+from replit import clear
 #first print all game material
 print(logo)
 game_continue = True
@@ -11,17 +12,15 @@ score = 0
 while game_continue:
     print(f"You are right! Current Score {score}")
     choice_one = random.choice(data)
-    print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
-
-    print(vs)
     choice_two = random.choice(data)
-
     if choice_one == choice_two:
         choice_two = random.choice(data)
+    print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']} ")
+    print(vs)
     print(f"Compare B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']} ")
-
     user_choice = input("Who has more follower Type : 'A' or 'B' ").capitalize()
-
+    clear()
+    print(logo)
     if choice_one['follower_count']>choice_two['follower_count'] and user_choice=="A":
         score += 1
     elif choice_one['follower_count']<choice_two['follower_count'] and user_choice=="B":
