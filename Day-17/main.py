9803184b5b3d5291a0e3b927fa3f932adeba36eb
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-21 16:02:06 +0300

    Adding Methods

--- Day-17/main.py
+++ Day-17/main.py
@@ -3,11 +3,28 @@ class User:
     def __init__(self, user_name, email):
         self.user_name = user_name
         self.email = email
-        self.follower = 0
+        self.followers = 0
+        self.following = 0
+# Adding Methods
+    def follower(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 
 userOne = User("Dagmawi", "dagmawiletarik@gmail.com")
 print(f"The user name: {userOne.user_name} \nEmail Address : {userOne.email} \nFollower : {userOne.follower}")
+
+userTwo = User("Abebe", "abebe@gmail.com")
+
+userOne.follower(userTwo)
+
+print(userOne.followers)
+print(userOne.following)
+print(userTwo.followers)
+print(userTwo.following)
+
+
 
 
 
