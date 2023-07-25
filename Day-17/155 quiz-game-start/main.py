41e616ee9346af1acd4ec59146a05de716ca668c
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-25 22:03:41 +0300

    Adding Number

--- Day-17/155 quiz-game-start/main.py
+++ Day-17/155 quiz-game-start/main.py
@@ -8,11 +8,11 @@ for question in question_data:
 
     question_text = question['text']
     question_answer = question['answer']
-    print(question_text,question_answer)
+    # print(question_text,question_answer)
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question.text)
 
-print(question_bank)
+# print(question_bank)
 
 quiz = QuizBrain(question_bank)
 
--- Day-17/155 quiz-game-start/quiz_brain.py
+++ Day-17/155 quiz-game-start/quiz_brain.py
@@ -4,5 +4,6 @@ class QuizBrain:
         self.question_list = q_list
     def next_question(self):
         current_question = self.question_list[self.question_number]
+        self.question_number += 1
         input(f"Q.{self.question_number}: {current_question} (True/False)")
 
