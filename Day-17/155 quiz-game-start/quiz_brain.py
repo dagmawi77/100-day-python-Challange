a936ba4adb055582a53d8765c8271a15059df4d6
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-26 21:53:24 +0300

    track current question

--- Day-17/155 quiz-game-start/quiz_brain.py
+++ Day-17/155 quiz-game-start/quiz_brain.py
@@ -1,3 +1,4 @@
+
 class QuizBrain:
     def __init__(self,q_list):
         self.question_number = 0
@@ -12,7 +13,7 @@ class QuizBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer=input(f"Q.{self.question_number}: {current_question} (True/False)")
-        self.check_answer(user_answer,current_question.answer)
+        self.check_answer(user_answer, current_question.)
     def check_answer(self, user_answer ,correct_answer):
         if user_answer.lower()== correct_answer.lower():
             print("You are right")
@@ -20,5 +21,5 @@ class QuizBrain:
         else:
             print("You are Wrong")
         print(f"The Correct answer was {correct_answer}")
-        
+        print(f"Your Current Score is : {self.score}/{self.question_number}")
 
