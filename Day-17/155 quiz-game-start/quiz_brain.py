5e3a52755a0234eceea73cc9ee7157dc902318b6
dagmawi77 <dagmawiletarik@gmail.com>
2023-07-26 21:34:18 +0300

    check question Functionality

--- Day-17/155 quiz-game-start/quiz_brain.py
+++ Day-17/155 quiz-game-start/quiz_brain.py
@@ -10,5 +10,12 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question} (True/False)")
+        user_answer=input(f"Q.{self.question_number}: {current_question} (True/False)")
+        self.check_answer(user_answer,current_question.answer)
+    def check_answer(self, user_answer ,current_question):
+        if user_answer.lower()== current_question.lower():
+            print("You are right")
+        else:
+            print("You are Wrong")
+        
 
