c5039ac3e4ce496016d9fa853656df76597c1e5d
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-02 22:43:23 +0300

    working with turtle class

--- Day-18/main.py
+++ Day-18/main.py
@@ -2,6 +2,19 @@ from turtle import Turtle,Screen
 
 timmy_the_turtle = Turtle()
 
+timmy_the_turtle.shape("circle")
+
+timmy_the_turtle.color("sea green")
+
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.backward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.backward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+
+
 screen = Screen()
 
 screen.exitonclick()
