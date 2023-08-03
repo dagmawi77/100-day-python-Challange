fb0bc5af0487f1756361151586dda14623948e2f
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-03 15:13:26 +0300

    draw square ui adding for loop

--- Day-18/main.py
+++ Day-18/main.py
@@ -2,16 +2,23 @@ from turtle import Turtle,Screen
 
 dagi_my_turtle = Turtle()
 
-dagi_my_turtle.shape("circle")
+dagi_my_turtle.shape("turtle")
 dagi_my_turtle.color("green")
-dagi_my_turtle.forward(100)
-dagi_my_turtle.left(90)
-dagi_my_turtle.forward(100)
-dagi_my_turtle.left(90)
-dagi_my_turtle.forward(100)
-dagi_my_turtle.left(90)
-dagi_my_turtle.forward(100)
-dagi_my_turtle.left(90)
+# I can do with for loop
+
+for _ in range(4):
+    dagi_my_turtle.forward(100)
+    dagi_my_turtle.left(90)
+
+# this is the junk step to complet the challange
+# dagi_my_turtle.forward(100)
+# dagi_my_turtle.left(90)
+# dagi_my_turtle.forward(100)
+# dagi_my_turtle.left(90)
+# dagi_my_turtle.forward(100)
+# dagi_my_turtle.left(90)
+# dagi_my_turtle.forward(100)
+# dagi_my_turtle.left(90)
 
 
 # dagi_my_turtle.left(90)
