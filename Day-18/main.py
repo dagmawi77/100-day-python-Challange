ff03ab27ea8eea1ea1c27a6f5e11a67b4a02963c
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-03 15:17:13 +0300

    refactor the object name

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,33 +1,33 @@
 from turtle import Turtle,Screen
 
-dagi_my_turtle = Turtle()
+dagi = Turtle()
 
-dagi_my_turtle.shape("turtle")
-dagi_my_turtle.color("green")
+dagi.shape("turtle")
+dagi.color("green")
 # I can do with for loop
 
 for _ in range(4):
-    dagi_my_turtle.forward(100)
-    dagi_my_turtle.left(90)
+    dagi.forward(100)
+    dagi.left(90)
 
 # this is the junk step to complet the challange
-# dagi_my_turtle.forward(100)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.forward(100)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.forward(100)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.forward(100)
-# dagi_my_turtle.left(90)
-
-
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.back(100)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.left(90)
-# dagi_my_turtle.forward(100)
+# dagi.forward(100)
+# dagi.left(90)
+# dagi.forward(100)
+# dagi.left(90)
+# dagi.forward(100)
+# dagi.left(90)
+# dagi.forward(100)
+# dagi.left(90)
+#
+# 
+# dagi.left(90)
+# dagi.left(90)
+# dagi.back(100)
+# dagi.left(90)
+# dagi.left(90)
+# dagi.left(90)
+# dagi.forward(100)
 
 
 screen = Screen()
