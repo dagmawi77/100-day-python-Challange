cea030940326f065bf1fa0bb8f808fe7fa495777
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-03 15:05:57 +0300

    draw square ui

---
+++ Day-18/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle,Screen
+
+dagi_my_turtle = Turtle()
+
+dagi_my_turtle.shape("circle")
+dagi_my_turtle.color("green")
+dagi_my_turtle.forward(100)
+dagi_my_turtle.left(90)
+dagi_my_turtle.forward(100)
+dagi_my_turtle.left(90)
+dagi_my_turtle.left(90)
+dagi_my_turtle.left(90)
+dagi_my_turtle.back(100)
+dagi_my_turtle.left(90)
+dagi_my_turtle.left(90)
+dagi_my_turtle.left(90)
+dagi_my_turtle.forward(100)
+
+
+screen = Screen()
+
+screen.exitonclick()
