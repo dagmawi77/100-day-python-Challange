13dc250627b7b13b191629c3ab21e41412a4fd84
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-03 15:45:46 +0300

    Challange 2 exercise

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,14 +1,27 @@
 from turtle import Turtle,Screen
 
+import heroes
+
 dagi = Turtle()
 
 dagi.shape("turtle")
 dagi.color("green")
 # I can do with for loop
+# Draw Rectangle
+# for _ in range(4):
+#     dagi.forward(100)
+#     dagi.left(90)
+#
+# Draw line Exercise
 
-for _ in range(4):
-    dagi.forward(100)
-    dagi.left(90)
+for _ in range(15):
+
+    dagi.pendown()
+    dagi.forward(10)
+    dagi.penup()
+    dagi.forward(10)
+
+
 
 # this is the junk step to complet the challange
 # dagi.forward(100)
@@ -20,7 +33,7 @@ for _ in range(4):
 # dagi.forward(100)
 # dagi.left(90)
 #
-# 
+#
 # dagi.left(90)
 # dagi.left(90)
 # dagi.back(100)
