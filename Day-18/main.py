66e8343f7207d987ac054ba88bf1b1d7a1671f3b
dagmawi77 <dagmawiletarik@gmail.com>
2023-08-03 16:19:08 +0300

    Draw random shape and color

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,4 +1,6 @@
+import random
 from turtle import Turtle,Screen
+from random import Random
 
 import heroes
 
@@ -13,14 +15,43 @@ dagi.color("green")
 #     dagi.left(90)
 #
 # Draw line Exercise
+#
+# for _ in range(15):
+#
+#     dagi.pendown()
+#     dagi.forward(10)
+#     dagi.penup()
+#     dagi.forward(10)
 
-for _ in range(15):
-
-    dagi.pendown()
-    dagi.forward(10)
-    dagi.penup()
-    dagi.forward(10)
-
+# Draw Different angle from rectangle to decagone
+# triangle = 3
+# angle = 360 / triangle
+#
+# for _ in range(triangle):
+#     dagi.forward(100)
+#     dagi.left(angle)
+#
+# rectangle = 4
+# angle = 360 / rectangle
+#
+# for _ in range(rectangle):
+#     dagi.forward(100)
+#     dagi.left(angle)
+# hexagon = 5
+# angle = 360 / hexagon
+#
+# for _ in range(hexagon):
+#     dagi.forward(100)
+#     dagi.left(angle)
+def draw_shape(number_side):
+    color = ["blue","aquamarine","chocolate","magenta","blue violet","orange"]
+    angle = 360 / number_side
+    for _ in range(number_side):
+        dagi.forward(100)
+        dagi.left(angle)
+    dagi.color(random.choice(color))
+for num in range(3,10):
+    draw_shape(num)
 
 
 # this is the junk step to complet the challange
